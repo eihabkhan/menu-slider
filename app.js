94874fac0f23dab4373239da388392a0f7079b4d
Eihab <mastereihab@gmail.com>
2020-08-11 16:10:57 +0100

    Added menu slide and modal pop up functionality

---
+++ app.js
@@ -0,0 +1,24 @@
+const toggle = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
+const openBtn = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("show-nav");
+});
+
+// Show modal
+openBtn.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("show-modal")
+})
+
+// Hide modal on outside click 
+window.addEventListener("click", (e) => {
+    e.target == modal ? modal.classList.remove("show-modal") : false
+})
